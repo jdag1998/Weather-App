2e6a31ab70034ebd556b66b233714f9a9d3c0afd
Juan Andrade <you@example.com>
2022-10-26 12:18:05 -0400

    Api Url Prepared

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+// const url ="https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=895284fb2d2c50a520ea537456963d9c"
+
+
+
+
   return (
     <div className="App">
-      
+      Hello
     </div>
   );
 }
